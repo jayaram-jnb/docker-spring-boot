03c5c1c8f95a637dc9656e4123d80328c93de408
jayaram-jnb <jayaramnayak227b@gmail.com>
2025-02-09 11:38:41 +0530

    Feb 10

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Kubernete Deployment using Helm, Jenkins Pipeline on Jan 06 2025");
+        model.addAttribute("title", "Kubernete Deployment using Helm, Jenkins Pipeline on Feb 10 2025");
         model.addAttribute("msg", "Deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
